4ecca42e867bc24dcb07b9d4ea8513f06871d512
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-10-03 21:59:07 +1100

    Finish ship factory function section of project.

--- src/ship.js
+++ src/ship.js
@@ -1,3 +1,37 @@
 export default function shipFactory(length) {
-    return {length}
+    const hull = [];
+    //Initalize hull
+    for(let i = 0; i < length; i++) hull.push('unhit');
+
+    function hit(pos) {
+        if(pos < 0 || pos >= length) {
+            return 'miss';
+        } else {
+            hull[pos] = 'hit';
+            checkIfSunk();
+            return 'hit';
+        }
+    }
+
+    function checkIfSunk() {
+        for(let pos = 0; pos < hull.length; pos++) {
+            if(hull[pos] == 'unhit') return;
+        }
+        sunk = true;
+    }
+
+    function getHull(pos) {
+        if(pos < 0 || pos >= length) {
+            return 'bad position';
+        } else {
+            return hull[pos];
+        }
+    }
+
+    let sunk = false;
+    function isSunk() {
+        return sunk;
+    }
+
+    return {length: hull.length, getHull, hit, isSunk};
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,8 +1,39 @@
 import shipFactory from "./ship.js"
 
 test("Ship length is 4", () => {
-    let ship = shipFactory(4);
+    const ship = shipFactory(4);
     expect(ship.length).toBe(4);
 });
 
-//test("End of ship is hit")
+test("Hit the end of the ship.", () => {
+    const ship = shipFactory(4);
+    ship.hit(3);
+    expect(ship.getHull(3)).toBe('hit');
+});
+
+test("Middle of ship is unhit.", () => {
+    const ship = shipFactory(5);
+    ship.hit(0);
+    ship.hit(1);
+    ship.hit(3);
+    ship.hit(4);
+    expect(ship.getHull(2)).toBe('unhit');
+});
+
+test("Ship is not sunk", () => {
+    const ship = shipFactory(2);
+    expect(ship.isSunk()).toBe(false);
+});
+
+test("Ship is sunk", () => {
+    const ship = shipFactory(2);
+    ship.hit(0);
+    ship.hit(1);
+    expect(ship.isSunk()).toBe(true);
+});
+
+test("Ship is also not sunk", () => {
+    const ship = shipFactory(2);
+    ship.hit(0);
+    expect(ship.isSunk()).toBe(false);
+});
