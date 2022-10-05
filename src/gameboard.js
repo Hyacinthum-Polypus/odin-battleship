e185301ff80a976683f570712964ec95b7dbd0d4
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-10-05 19:02:17 +1100

    Update factory functions to properly use prototypes.

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,3 +1,55 @@
+const gameBoardPrototype = {
+    place(x, y, ship, axis) {
+        if(x < 0 || x > 9 || y < 0 || y > 9) return "Fail: Ship does not fit on gameboard.";
+        switch(axis) {
+            case 'x':
+                for(let i = 0; i < ship.length; i++) {
+                    if(x + i > 9) return "Fail: Ship does not fit on gameboard.";
+                    else if(this.grid[x+i][y].state != 'none') return "Fail: ship already on square (" + (x+i) + ", " + y + ")"
+                }
+                for(let i = 0; i < ship.length; i++) {
+                    this.grid[x+i][y].state = ship;
+                    this.grid[x+i][y].length = i;
+                }
+            break;
+            case 'y':
+                for(let i = 0; i < ship.length; i++) {
+                    if(y + i > 9) return "Fail: Ship does not fit on gameboard."
+                    if(this.grid[x][y+i].state != 'none') return "Fail: ship already on square (" + x + ", " + (y+i) + ")"
+                }
+                for(let i = 0; i < ship.length; i++) {
+                    this.grid[x][y+i].state = ship;
+                    this.grid[x][y+i].length = i;   
+                }
+            break;
+        }
+        this.ships.push(ship);
+    },
+    getSquare(x, y) {
+        return this.grid[x][y];
+    },
+    receiveAttack(x, y) {
+        if(this.grid[x][y].state != 'none')
+        {
+            this.grid[x][y].state.hit(this.grid[x][y].length);
+        } else {
+            this.addMiss(x, y);
+        }
+    },
+    addMiss(x, y) {
+        this.misses.push([x, y]);
+    },
+    isAllShipsSunk() {
+        for(let element = 0; element < this.ships.length; ++element) {
+            const ship = this.ships[element];
+            if(!ship.isSunk()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
 export default function gameBoardFactory() {
     const grid = [
         [{state:'none'},{state:'none'},{state:'none'},{state:'none'},{state:'none'},{state:'none'},{state:'none'},{state:'none'},{state:'none'},{state:'none'},],
@@ -14,59 +66,5 @@ export default function gameBoardFactory() {
     const misses = [];
     const ships = [];
 
-    function place(x, y, ship, axis) {
-        if(x < 0 || x > 9 || y < 0 || y > 9) return "Fail: Ship does not fit on gameboard.";
-        switch(axis) {
-            case 'x':
-                for(let i = 0; i < ship.length; i++) {
-                    if(x + i > 9) return "Fail: Ship does not fit on gameboard.";
-                    else if(grid[x+i][y].state != 'none') return "Fail: ship already on square (" + (x+i) + ", " + y + ")"
-                }
-                for(let i = 0; i < ship.length; i++) {
-                    grid[x+i][y].state = ship;
-                    grid[x+i][y].length = i;
-                }
-            break;
-            case 'y':
-                for(let i = 0; i < ship.length; i++) {
-                    if(y + i > 9) return "Fail: Ship does not fit on gameboard."
-                    if(grid[x][y+i].state != 'none') return "Fail: ship already on square (" + x + ", " + (y+i) + ")"
-                }
-                for(let i = 0; i < ship.length; i++) {
-                    grid[x][y+i].state = ship;
-                    grid[x][y+i].length = i;   
-                }
-            break;
-        }
-        ships.push(ship);
-    }
-
-    function getSquare(x, y) {
-        return grid[x][y];
-    }
-
-    function receiveAttack(x, y) {
-        if(grid[x][y].state != 'none')
-        {
-            grid[x][y].state.hit(grid[x][y].length);
-        } else {
-            addMiss(x, y);
-        }
-    }
-
-    function addMiss(x, y) {
-        misses.push([x, y]);
-    }
-
-    function isAllShipsSunk() {
-        for(let element = 0; element < ships.length; ++element) {
-            const ship = ships[element];
-            if(!ship.isSunk()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    return {place, getSquare, receiveAttack, misses, isAllShipsSunk}
+    return Object.assign(Object.create(gameBoardPrototype), {grid, misses, ships});
 }
--- src/ship.js
+++ src/ship.js
@@ -1,37 +1,35 @@
+const shipPrototype = {
+    hit(pos) {
+        if(pos < 0 || pos >= this.length) {
+            return 'miss';
+        } else {
+            this.hull[pos] = 'hit';
+            this.checkIfSunk();
+            return 'hit';
+        }
+    },
+    checkIfSunk() {
+        for(let pos = 0; pos < this.hull.length; pos++) {
+            if(this.hull[pos] == 'unhit') return;
+        }
+        this.sunk = true;
+    },
+    getHull(pos) {
+        if(pos < 0 || pos >= this.length) {
+            return 'bad position';
+        } else {
+            return this.hull[pos];
+        }
+    },
+    isSunk() {
+        return this.sunk;
+    }
+}
+
 export default function shipFactory(length) {
     const hull = [];
     //Initalize hull
     for(let i = 0; i < length; i++) hull.push('unhit');
-
-    function hit(pos) {
-        if(pos < 0 || pos >= length) {
-            return 'miss';
-        } else {
-            hull[pos] = 'hit';
-            checkIfSunk();
-            return 'hit';
-        }
-    }
-
-    function checkIfSunk() {
-        for(let pos = 0; pos < hull.length; pos++) {
-            if(hull[pos] == 'unhit') return;
-        }
-        sunk = true;
-    }
-
-    function getHull(pos) {
-        if(pos < 0 || pos >= length) {
-            return 'bad position';
-        } else {
-            return hull[pos];
-        }
-    }
-
     let sunk = false;
-    function isSunk() {
-        return sunk;
-    }
-
-    return {length: hull.length, getHull, hit, isSunk};
+    return Object.assign(Object.create(shipPrototype), {length: hull.length, hull, sunk});
 }
