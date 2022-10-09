48b4c3ebbf50b528adfa930dd6f505baf45f13a3
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-10-09 22:17:15 +1100

    Add css for target and placement grid.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import './style.css';
 
-function test() {
-    const test = document.createElement('h1');
-    test.textContent = "Hello, world!";
-    test.classList.add('hello');
-    return test;
+function grid() {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    for (let index = 0; index < 100; index++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square);
+    }
+    return grid;
 }
 
-document.body.appendChild(test());
+function gridSmall() {
+    const grid = document.createElement('div');
+    grid.classList.add('grid-small');
+    for (let index = 0; index < 100; index++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        grid.appendChild(square);
+    }
+    return grid;
+}
+
+document.body.appendChild(grid());
+document.body.appendChild(gridSmall());
