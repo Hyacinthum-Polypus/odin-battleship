f9ad2cfe66d578f1666e2f9728e04b7f3f079f30
Hyacinthum-Polypus <18484965+Hyacinthum-Polypus@users.noreply.github.com>
2022-10-10 18:18:47 +1100

    Add x and y attributes to dom squares.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import './style.css';
 
+function square(x, y) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.setAttribute("data-x", x);
+    square.setAttribute("data-y", y)
+    return square;
+}
+
 function grid() {
     const grid = document.createElement('div');
     grid.classList.add('grid');
-    for (let index = 0; index < 100; index++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        grid.appendChild(square);
+    for (let y = 0; y < 10; y++) {
+        for(let x = 0; x < 10; x++) {;
+            grid.appendChild(square(x, y));
+        }
     }
     return grid;
 }
@@ -14,10 +22,10 @@ function grid() {
 function gridSmall() {
     const grid = document.createElement('div');
     grid.classList.add('grid-small');
-    for (let index = 0; index < 100; index++) {
-        const square = document.createElement('div');
-        square.classList.add('square');
-        grid.appendChild(square);
+    for (let y = 0; y < 10; y++) {
+        for(let x = 0; x < 10; x++) {;
+            grid.appendChild(square(x, y));
+        }
     }
     return grid;
 }
